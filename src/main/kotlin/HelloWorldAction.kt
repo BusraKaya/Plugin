540cdc71184999c26928ed506db5eba81f8c5629
Busra Kaya <busrakaya0697@hotmail.com>
2024-07-11 23:29:20 +0300

    first plugin

---
+++ src/main/kotlin/HelloWorldAction.kt
@@ -0,0 +1,7 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class HelloWorldAction: AnAction() {
+    override fun actionPerformed(p0: AnActionEvent) {
+    }
+}
